5ccff1838858a8d5476f5cb335bb1a643441e912
John Dekis <johndekis@gmail.com>
2018-02-05 18:51:00 -0500

    consolidated logic and html files

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,66 +1,63 @@
-$(document).ready(function () {
-
-$("#resultDisplay").hide();
-
-$("#submitBtn").on("click", function (event) {
-    event.preventDefault();
-    $("#resultDisplay").show();
-});
-
-});
-
-
-//working on hiding everthing except for the log in button, click that shows a modal for username, then the main page
-
-    $(document).ready(function() {
-
-
-    var ingredient = $('#ingredient');
-    var healthFlag = $('#health');
-
-    $("#logInModal").hide();
-
-    $("#logIn").on("click", function() {
-        console.log("clicked");
-        $("#startBox").hide();
-        $("#logInModal").show();
-        
-   });
-
-    $('#submitBtn').on('click', function(e) {
-        e.preventDefault();
-        console.log(ingredient.val());
-        console.log(healthFlag).val();
-      
-      $('#recipe-display').empty();
-      
-      
-        var wellDiv = $('<div></div>');
-        wellDiv.addClass('well');
-        wellDiv.attr('id', 'recipeWell');
+$(document).ready(function() {
+    
+    
+        var ingredient = $('#ingredient');
         
         
-        $('#recipeDisplay').append(wellDiv);
-        wellDiv.text('<h2>' + ingredient + '</h2>');
-        console.log(wellDiv);
-        
-        
-
-         });
-       
-
-    });
-   
-   
-   $(".close").on("click", function() {
+        $("#mainScreen").hide();
         $("#logInModal").hide();
-        $("#startBox").show();
+        $("startBox").show();
+        
+        
+        $("#logInBtn").on("click", function() {
+            console.log("clicked");
+            $("#startBox").hide();
+            $("#logInModal").show();
+           
+       }); 
+       
+       
+       $(".close").on("click", function() {
+            $("#logInModal").hide();
+            $("#startBox").show();
+    
+       });
+       
+       
+       $("#logInSubmit").on("click", function(e) {
+            e.preventDefault();
+            var userName = $("#userName").val().trim();
+            console.log("Welcome, " + userName + "!");
+            $("#mainScreen").show();
+            $("#startBox").hide();
+            $("#logInModal").hide();
+       });
+    
+       
+       // 
+        $('#submitBtn').on('click', function(e) {
+            e.preventDefault();
 
-   });
-   
-   $("#logInSubmit").on("click", function(e) {
-    var userName = $("#userName").val();
-        console.log(userName);
-   });
+            //ajax call
 
-});
+            console.log(ingredient.val());
+            
+            //empties current display
+            $('#recipe-display').empty();
+          
+            //div to fill with info from results
+            var wellDiv = $('<div>');
+            wellDiv.attr('id', 'recipe-0');
+            wellDiv.html("<h3>" + ingredient.val() + "</h3>");
+            
+           
+            //append div to display
+            $('#recipe-display').append(wellDiv);
+            //append recipe image
+            
+    
+        });
+    
+    
+    
+    });
