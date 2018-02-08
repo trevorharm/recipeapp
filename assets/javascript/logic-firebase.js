3d636ca1ae7a33c17f093b77c32bf33b7d1ac16c
George Greene <georgegreene@outlook.com>
2018-02-07 20:47:46 -0500

    updated firebase

--- assets/javascript/logic-firebase.js
+++ assets/javascript/logic-firebase.js
@@ -1,43 +1,44 @@
-  // Initialize Firebase
-  var config = {
+// Initialize Firebase
+var config = {
     apiKey: "AIzaSyC7SQ244UWpfL-WnGQX5SmW4pMlYo-_aCw",
     authDomain: "recipe-app-e904e.firebaseapp.com",
     databaseURL: "https://recipe-app-e904e.firebaseio.com",
     projectId: "recipe-app-e904e",
     storageBucket: "",
     messagingSenderId: "319710210801"
-  };
-  firebase.initializeApp(config);
+    };
+    firebase.initializeApp(config);
 
-  var database = firebase.database();
-  var name = "";
-  var recipes = [];
-  var video = "";
+    var database = firebase.database();
 
-  $("#submitBtn").on("click", function(event) {
-      event.preventDefault();
-      name = "Sam";
-      recipes.push("Chicken Paprika");
-      recipes.push("Chicken Nuggets");
-      recipes.push("Chicken Pot Pie");
-      recipes.push("Chicken Dumplings");
-      recipes.push("Chicken Fingers");
-      video = "Watch: How to Make Chicken Paprika";
-      var ingredient = $("#ingredient").val().trim();
-    //   console.log(ingredient);
-      database.ref().push({
-          name: name,
-          ingredient: ingredient,
-          recipe: recipes,
-          video: video
-      });
-  });
+    var newUser = {
+    name: "",
+    ingredient: "",
+    recipe: "",
+    video: ""
+    };
 
-  database.ref().on("child_added", function(childSnapshot) {
-      console.log(childSnapshot.val().name);
-      console.log(childSnapshot.val().ingredient);
-      console.log(childSnapshot.val().recipe);
-      console.log(childSnapshot.val().video);
-  }, function(errorObject) {
-      console.log("Errors handled: " + errorObject.code);
-  });
+    $("#logInSubmit").on("click", function(event) {
+    event.preventDefault();
+    newUser.name = $("#userName").val().trim();
+    console.log(newUser.name);
+    });
+
+    $("#submitBtn").on("click", function(event) {
+    event.preventDefault();
+    newUser.ingredient = $("#ingredient").val().trim();
+    console.log(newUser.ingredient);
+    database.ref('users').child.push(newUser);
+    });
+
+    // $("#favoriteBtn").on("click", function(event) {
+    // event.preventDefault();
+    // newUser.recipe = results.hits[?]
+    // database.ref('users').push(newUser);
+    // });
+
+    database.ref().on("child_added", function(childSnapshot) {
+    console.log(childSnapshot.val().newUser);
+    }, function(errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+    });
