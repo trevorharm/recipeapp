72139eee825a5cf171ab3b4839f4ae55df8c7b13
John Dekis <johndekis@gmail.com>
2018-02-10 11:08:45 -0500

    close buttons on modals

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,3 +1,5 @@
+
+
 function valid() {
     var string = $("#inputEmail").val();
     if(string === "") {
@@ -7,7 +9,7 @@ function valid() {
 
     var substring1 = "@";
     var substring2 = ".com";
-    
+     //runs valid function to check input for "@" and ".com" strings before enabling button
     if(string.indexOf(substring1) !== -1 && string.indexOf(substring2) !== -1) {
         $("#logInSubmit").prop("disabled", false);
         console.log(string);
@@ -29,42 +31,66 @@ function slideTime() {
     setTimeout(function(){ $("#logInModal").slideDown(); }, 500);
 }
 
+function signUpSlide() {
+    setTimeout(function(){ $("#signUpModal").slideDown(); }, 500);
+}
+
 function slideTime2() {
     setTimeout(function(){ $("#startBox").slideDown(); }, 500);
 }
 
 $(document).ready(function() {
            
-    
+        var email;
         var ingredient = $('#ingredient');
         var input = $("#inputEmail");
+        var signUp = $("#signUpEmail");
        
         $("startBox").show();
-               
+         //log in click event       
         $("#logInBtn").on("click", function() {
             console.log("clicked");
             $("#startBox").slideUp();
             slideTime();
-            valid();                          
+           valid();                          
        }); 
+
+
+       $("#signUpBtn").on("click", function() {
+        console.log("clicked");
+        $("#startBox").slideUp();
+        signUpSlide();
+       valid();                          
+   }); 
        
+    //validates text on change and keyup
        input.on("change", valid);
-       input.on("keyup", valid); //on change here? makes you focus or unfocus to run valid();
+       input.on("keyup", valid); 
+       signUp.on("change", valid);
+       signUp.on("keyup", valid);
       
-       $(".close").on("click", function() {
+       $(".close1").on("click", function() {
             $("#logInModal").slideUp();
            slideTime2();
             
     
        });
 
-       //runs valid function to check input for "@" and ".com" strings before enabling button
+       $(".close2").on("click", function() {
+        $("#signUpModal").slideUp();
+       slideTime2();
+        
+
+     });
+
+      
        
        
+      
        $("#logInSubmit").on("click", function(e) {
             e.preventDefault();
             
-            var email = $("#inputEmail").val().trim();
+            email = $("#inputEmail").val().trim();
             console.log("Welcome, " + email + "!");
             $("#mainScreen").slideDown();
             $("#searchRow").slideDown();
@@ -73,6 +99,18 @@ $(document).ready(function() {
             $("#startBox").hide();
             $("#logInModal").hide();
        });
+
+       // sign up submission click event
+       $("#signUpSubmit").on("click", function(e) {
+        e.preventDefault();
+        
+        email = $("#signUpEmail").val().trim();
+        console.log("Welcome, " + email + "!");
+        $("#mainScreen").slideDown();
+        $("#searchRow").slideDown();
+        $("#startBox").hide();
+        $("#signUpModal").hide();
+        });
     
        
        // 
